f86b6b178d01aa009bf9cb6e7906f7918f3084f7
Cragg <tomaszrogalski11@gmail.com>
2022-03-28 14:51:39 +0200

    update topic model(title, text, dates)

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Topic(models.Model):
-    pass
+    title = models.CharField(max_length=30)
+    text = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
 
 class Comment(models.Model):
     pass

16f08910e59280d7ca04df3ccda727957e915c20
Cragg <tomaszrogalski11@gmail.com>
2022-03-31 11:37:28 +0200

    coments base func update

--- blogapp/models.py
+++ blogapp/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 class Topic(models.Model):
     title = models.CharField(max_length=30)
+    description = models.CharField(max_length=50)
     text = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
 
 class Comment(models.Model):
-    pass
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200, default='')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)

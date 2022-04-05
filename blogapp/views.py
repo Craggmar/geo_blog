cf8e4851dc2e2b734709fd911346cd31d3020e9c
Cragg <tomaszrogalski11@gmail.com>
2022-04-05 13:04:04 +0200

    single topic view & template

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,16 @@
+from datetime import date
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Topic
+
+def index(request):
+    topics = Topic.objects.order_by('date_created')
+    context = {'topics': topics}
+    return render(request, 'blogapp/index.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id = topic_id)
+    comments = topic.comment_set.order_by('-date_created')
+    context = {'topic': topic, 'comments': comments}
+    return render(request, 'blogapp/topic.html', context)
+    
